c387ed31b51f004bcf51cb1467f2a7c8652f7323
AZ <47402666+realazaber@users.noreply.github.com>
2025-04-29 02:06:47 +1000

    UI: Nav

---
+++ frontend/src/components/universal/Nav.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <nav class="px-3 py-2 flex justify-between items-center">
+    <RouterLink to="/"
+      ><img src="../../assets/logo.webp" alt="logo" class="object-contain h-12"
+    /></RouterLink>
+  </nav>
+</template>
+
+<style lang="scss" scoped></style>
