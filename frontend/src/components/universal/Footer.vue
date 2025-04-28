cec592048be62bbe2e5e14e9756ff3b86392279a
AZ <47402666+realazaber@users.noreply.github.com>
2025-04-29 02:06:42 +1000

    UI: Footer

---
+++ frontend/src/components/universal/Footer.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const currentYear: number = new Date().getFullYear();
+</script>
+
+<template>
+  <footer
+    class="mt-auto w-full flex justify-center items-center bg-primary text-white p-2"
+  >
+    <a href="https://azaber.com/" target="_blank"
+      >Made by {{ currentYear }} Azaber</a
+    >
+  </footer>
+</template>
+
+<style lang="scss" scoped></style>
